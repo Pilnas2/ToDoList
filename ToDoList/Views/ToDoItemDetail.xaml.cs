0b8da507f2881fc5685bc8d850d3f002bb91c0b4
Martin Pilný <martin.pilnas@email.cz>
2024-04-21 16:13:02 +0200

    update items

--- ToDoList/Views/ToDoItemDetail.xaml.cs
+++ ToDoList/Views/ToDoItemDetail.xaml.cs
@@ -9,4 +9,27 @@ public partial class ToDoItemDetail : ContentPage
         InitializeComponent();
         BindingContext = item;
     }
+
+    async void UpdateItemAsync(object sender, EventArgs e)
+    {
+        var todoItem = (ToDoItem)BindingContext;
+        TodoItemDatabase database = await TodoItemDatabase.Instance;
+        await database.SaveItemAsync(todoItem);
+        await Navigation.PushAsync(new TodoItemPage());
+    }
+    async void DeleteItemAsync(object sender, EventArgs e)
+    {
+        var todoItem = (ToDoItem)BindingContext;
+        if (todoItem is null) return;
+        TodoItemDatabase database = await TodoItemDatabase.Instance;
+        await database.DeleteItemAsync(todoItem);
+        await Navigation.PushAsync(new TodoItemPage());
+    }
+    //void InitalizeChecker(ToDoItem item)
+    //{
+    //    if (item.DueDate.HasValue)
+    //    {
+    //        reminderDatePickerEdit = new DateTime(item.DueDate);
+    //    }
+    //}
 }
